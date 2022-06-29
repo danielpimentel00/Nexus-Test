87cb3c631f90206fb8feea9e5b8deaee7988b3b4
danielpimentel00 <danielpimentel19@gmail.com>
2022-06-29 13:18:39 -0400

    feat: get transactions method created

---
+++ src/Services/TransactionsService.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+
+class TransactionsService {
+  _axiosInstance;
+
+  constructor() {
+    const axiosInstance = axios.create({
+      baseURL: process.env.REACT_APP_BASE_URL,
+      headers: { Authorization: process.env.REACT_APP_USER_ID },
+    });
+
+    this._axiosInstance = axiosInstance;
+  }
+
+  getTransactions() {
+    return new Promise((resolve, reject) => {
+      this._axiosInstance
+        .get("/transactions")
+        .then((res) => {
+          resolve(res.data.data);
+        })
+        .catch((err) => {
+          reject(err);
+        });
+    });
+  }
+}
+
+var transactionsService = new TransactionsService();
+export default transactionsService;
